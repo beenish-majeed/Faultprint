baed0d205719bb77e2b1eafda09511ab76afc90f
beenish-majeed <beenishmajeed33@gmail.com>
2026-09-10 15:47:54 +0500

    feat: add initial user interaction

---
+++ main.py
@@ -0,0 +1,13 @@
+print("\n                  SYSTEM ANALYSIS                  ")
+print("_" * 50)
+
+print("\nProblem")
+problem = input("Enter your issue: ").strip()
+
+print("\n1. Another issue")
+print("2. Exit")
+
+choice = input("Enter your choice: ").strip()
+
+if choice == "2": 
+    print("\nThanks for using!")
